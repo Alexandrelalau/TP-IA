61b8ad9b5f35cb3c0819c83fab05c2f42d23cf6e
AlexandreLALAU <alexandre.lalau99@gmail.com>
2020-12-03 20:47:12 +0100

    EX1.4

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -11,6 +11,12 @@ from sklearn import datasets
 import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
+import sklearn.metrics as sm
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from sklearn import datasets
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
 
@@ -41,9 +47,6 @@ df.drop('species',1,inplace=True)
 print(df.columns)
 
 #3
-
-
-
 X = iris.data
 y = iris.target
 target_names = iris.target_names
@@ -57,3 +60,29 @@ for color, i, target_name in zip(colors, [0, 1, 2], target_names):
 plt.legend(loc='best', shadow=False, scatterpoints=1)
 plt.title('PCA of IRIS dataset')
 plt.show()
+
+#4
+#J'ai préféré décomposé le travail que de directement faire cl=kmeans(data,nbclusters).
+
+iris = datasets.load_iris()
+#importer le jeu de données Iris dataset à l'aide du module pandas
+x = pd.DataFrame(iris.data)
+x.columns = ['Sepal_Length','Sepal_width','Petal_Length','Petal_width']
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+#Création d'un objet K-Means avec un regroupement en 3 clusters (groupes)
+model=KMeans(n_clusters=3)
+#application du modèle sur notre jeu de données Iris
+model.fit(x)
+
+colormap=np.array(['Red','green','blue'])
+#Visualisation du jeu de données sans altération de ce dernier (affichage des fleurs selon leur étiquettes)
+plt.scatter(x.Petal_Length, x.Petal_width,c=colormap[y.Targets],s=40)
+plt.title('Classification réelle')
+plt.show()
+
+#Visualisation des clusters formés par K-Means
+plt.scatter(x.Petal_Length, x.Petal_width,c=colormap[model.labels_],s=40)
+plt.title('Classification K-means ')
+plt.show()
+
