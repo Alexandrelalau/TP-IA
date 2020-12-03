05c9d938c708c0c5cb05732aaf1c7c0b554b1e6d
AlexandreLALAU <alexandre.lalau99@gmail.com>
2020-12-03 20:01:21 +0100

    EX1.3 finit en 2D

--- LAB4/LAB4.py
+++ LAB4/LAB4.py
@@ -8,6 +8,11 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import decomposition
 from sklearn import datasets
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.decomposition import PCA
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
 
 iris = load_iris()
 
@@ -38,35 +43,17 @@ print(df.columns)
 #3
 
 
-np.random.seed(5)
 
-centers = [[1, 1], [-1, -1], [1, -1]]
-iris = datasets.load_iris()
 X = iris.data
 y = iris.target
-
-fig = plt.figure(1, figsize=(4, 3))
-plt.clf()
-ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
-
-plt.cla()
-pca = decomposition.PCA(n_components=3)
-pca.fit(X)
-X = pca.transform(X)
-
-for name, label in [('Setosa', 0), ('Versicolour', 1), ('Virginica', 2)]:
-    ax.text3D(X[y == label, 0].mean(),
-              X[y == label, 1].mean() + 1.5,
-              X[y == label, 2].mean(), name,
-              horizontalalignment='center',
-              bbox=dict(alpha=.5, edgecolor='w', facecolor='w'))
-# Reorder the labels to have colors matching the cluster results
-y = np.choose(y, [1, 2, 0]).astype(np.float)
-ax.scatter(X[:, 0], X[:, 1], X[:, 2], c=y, cmap=plt.cm.nipy_spectral,
-           edgecolor='k')
-
-ax.w_xaxis.set_ticklabels([])
-ax.w_yaxis.set_ticklabels([])
-ax.w_zaxis.set_ticklabels([])
-
+target_names = iris.target_names
+pca = PCA(n_components=2)
+X_r = pca.fit(X).transform(X)
+colors = ['navy', 'turquoise', 'darkorange']
+lw = 2
+for color, i, target_name in zip(colors, [0, 1, 2], target_names):
+    plt.scatter(X_r[y == i, 0], X_r[y == i, 1], color=color, alpha=.8, lw=lw,
+                label=target_name)
+plt.legend(loc='best', shadow=False, scatterpoints=1)
+plt.title('PCA of IRIS dataset')
 plt.show()
